8d00c49a08afc1003d0316e5660078934b8a9262
njchensl <nanjingchj@gmail.com>
2020-03-30 14:25:37 -0400

    Set up basic application and entry point

---
+++ Sandbox/src/SandboxApp.cpp
@@ -0,0 +1,18 @@
+#include "Hazel.h"
+
+class Sandbox : public Hazel::Application
+{
+public:
+    Sandbox()
+    {
+    }
+
+    ~Sandbox() override
+    {
+    }
+};
+
+Hazel::Application* Hazel::CreateApplication()
+{
+    return new Sandbox;
+}
