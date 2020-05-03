f4e58bc1ff8c99dbd9292175158c79b01ab47124
njchensl <nanjingchj@gmail.com>
2020-05-03 11:45:34 -0400

    ground block fading effect

--- GeometryDash/assets/shaders/Texture.glsl
+++ GeometryDash/assets/shaders/Texture.glsl
@@ -16,6 +16,7 @@ out vec2 v_TexCoord;
 out vec4 v_Color;
 out float v_TexIndex;
 out float v_TilingFactor;
+out float v_XPos;
 
 void main() {
     v_TexCoord = a_TexCoord;
@@ -23,6 +24,7 @@ void main() {
     v_TexIndex = a_TexIndex;
     v_TilingFactor = a_TilingFactor;
     gl_Position = u_ViewProjection * vec4(a_Position, 1.0);
+    v_XPos = gl_Position.x;
     //v_Position = a_Position;
 }
 
@@ -37,6 +39,7 @@ in vec4 v_Color;
 in vec2 v_TexCoord;
 in float v_TexIndex;
 in float v_TilingFactor;
+in float v_XPos;
 
 //uniform float u_TilingFactor;
 uniform sampler2D u_Textures[32];
@@ -45,6 +48,9 @@ void main() {
     vec4 texColor = texture(u_Textures[int(v_TexIndex)], v_TexCoord * v_TilingFactor);
     if (texColor == vec4(1.0f, 0.0f, 1.0f, 1.0f)) {
         color = vec4(1.0f);
+    }
+    else if (texColor.x == 1.0f && texColor.y == 0.0f && texColor.w == 1.0f && texColor.z - 0.99f < 0.000000000000001f) {
+        color = vec4(1.0f, 1.0f, 1.0f, 1.0f - abs(v_XPos));
     }
     else {
         color = texColor * v_Color;
